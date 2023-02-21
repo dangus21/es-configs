ed2dcca9a3cef56f1f55bde76865a1bbbadff104
miguel magalhaes <ext.miguel.magalhaes@farfetch.com>
2023-02-21 17:17:58 +0000

    Remove react settings

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,11 +5,11 @@ module.exports = {
 		"node": true
 	},
 	"extends": [
-		"eslint:recommended",
 		"plugin:react/recommended",
+		"eslint:recommended",
 		"plugin:import/errors",
 		"plugin:import/warnings",
-		"plugin:import/typescript",
+		"plugin:import/typescript"
 	],
 	parserOptions: {
 		ecmaFeatures: {
@@ -21,26 +21,32 @@ module.exports = {
 	},
 	plugins: [
 		"react",
+		"react-hooks",
 		"unused-imports",
 		"babel",
 		"sort-imports-es6-autofix",
-		"react-hooks",
 		"import"
 	],
 	rules: {
 		quotes: [2, "double", { "avoidEscape": true }],
 		indent: ["error", "tab"],
 		semi: ["error", "always"],
+		"react/react-in-jsx-scope": "off",
+		"react/jsx-uses-react": "error",
+		"react/jsx-uses-vars": "error",
+		"react/prop-types": 0,
+		"react-hooks/rules-of-hooks": "error",
+		"react-hooks/exhaustive-deps": "warn",
+		"react/jsx-curly-brace-presence": [
+			"error",
+			{ "props": "never", "children": "never" }
+		],
 		"no-multiple-empty-lines": ["error", { "max": 1 }],
 		"no-trailing-spaces": ["error"],
 		"no-use-before-define": "error",
 		"prefer-const": "error",
-		"react/react-in-jsx-scope": "off",
 		"comma-dangle": ["error", "never"],
 		"linebreak-style": ["error", "unix"],
-		"react/jsx-uses-react": "error",
-		"react/jsx-uses-vars": "error",
-		"react/prop-types": 0,
 		"no-duplicate-imports": 2,
 		"sort-imports-es6-autofix/sort-imports-es6": [
 			2,
@@ -51,31 +57,20 @@ module.exports = {
 			}
 		],
 		"no-unused-vars": [2, { "ignoreRestSiblings": true }],
-		"react-hooks/rules-of-hooks": "error",
-		"react-hooks/exhaustive-deps": "warn",
 		"no-dupe-class-members": ["error"],
 		"import/no-extraneous-dependencies": [
 			"error",
 			{ "devDependencies": true }
 		],
-		"prefer-template": ["error"],
+		"prefer-template": ["error"]
 		// "no-console": ["error"],
-		"react/jsx-curly-brace-presence": [
-			"error",
-			{ "props": "never", "children": "never" }
-		]
 	},
 	overrides: [
 		{
 			files: ["*.ts", "*.tsx"],
 			extends: ["plugin:import/typescript"],
 			parser: "@typescript-eslint/parser",
-			plugins: ["@typescript-eslint"],
+			plugins: ["@typescript-eslint"]
 		}
-	],
-	settings: {
-		react: {
-			version: "detect"
-		}
-	}
-}
+	]
+};
